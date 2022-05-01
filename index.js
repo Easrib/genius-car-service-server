e0ec36235177271d8a4ec13773a359e91dec3cd8
Easrib <easrib.sharik01@gmail.com>
2022-05-01 22:02:15 +0600

    order collection

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("service");
+        const orderCollection = client.db("geniusCar").collection("order");
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -44,6 +45,12 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
+        })
+        // order collection API
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
         })
 
     } finally {
